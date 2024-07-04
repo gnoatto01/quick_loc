5e8e577033590424f73e773dc8e1252828e5165f
Fernando Augusto Gnoatto <fernn65@hotmail.com>
2024-07-04 14:21:16 -0300

    projeto inicial

---
+++ src/main/java/com/br/soluctions/attos/quick_loc/QuickLocApplication.java
@@ -0,0 +1,13 @@
+package com.br.soluctions.attos.quick_loc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickLocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickLocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/soluctions/attos/quick_loc/QuickLocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.soluctions.attos.quick_loc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickLocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
