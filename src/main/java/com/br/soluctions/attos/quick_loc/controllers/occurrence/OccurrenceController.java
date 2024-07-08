bc50ad0c62e1371d1ec49deb491b7cc8c280a64c
Fernando Augusto Gnoatto <fernn65@hotmail.com>
2024-07-08 15:46:39 -0300

    Service de ocorrencias criado, faltando apenas listar

--- src/main/java/com/br/soluctions/attos/quick_loc/controllers/occurrence/OccurrenceController.java
+++ src/main/java/com/br/soluctions/attos/quick_loc/controllers/occurrence/OccurrenceController.java
@@ -1,71 +1,48 @@
 package com.br.soluctions.attos.quick_loc.controllers.occurrence;
 
-import java.util.UUID;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.br.soluctions.attos.quick_loc.controllers.dto.occurrence.CreateOccurrenceDto;
-import com.br.soluctions.attos.quick_loc.entities.occurrence.Occurrence;
-import com.br.soluctions.attos.quick_loc.repositories.occurrence.OccurrenceRepository;
-import com.br.soluctions.attos.quick_loc.repositories.user.UserRepository;
+import com.br.soluctions.attos.quick_loc.services.occurrence.OccurrenceService;
 
 @RestController
 public class OccurrenceController {
-    private final OccurrenceRepository occurrenceRepository;
-    private final UserRepository userRepository;
+    private final OccurrenceService occurrenceService;
 
-    public OccurrenceController(OccurrenceRepository occurrenceRepository, UserRepository userRepository) {
-        this.occurrenceRepository = occurrenceRepository;
-        this.userRepository = userRepository;
+    public OccurrenceController(OccurrenceService occurrenceService) {
+        this.occurrenceService = occurrenceService;
     }
 
     @PostMapping("/occurrences")
     public ResponseEntity<Void> createOccurrence(@RequestBody CreateOccurrenceDto createOccurrenceDto,
             JwtAuthenticationToken token) {
 
-        var user = userRepository.findById(UUID.fromString(token.getName())); // pega o subject que foi passado no token
-
-        var occurrence = new Occurrence();
-
-        occurrence.setUser(user.get());
-        occurrence.setOcurrenceDescription(createOccurrenceDto.occurrenceDescription());
-        occurrence.setWriteLocation(createOccurrenceDto.writeLocation());
-        occurrence.setLatitude(createOccurrenceDto.latitude());
-        occurrence.setLongitude(createOccurrenceDto.longitude());
-        occurrence.setOccurreceStatus(createOccurrenceDto.occurrenceStatus());
-        occurrence.setOccurrencePhoto(createOccurrenceDto.occurrencePhoto());
-        occurrence.setOccurrencePriority(createOccurrenceDto.occurrencePriority());
-        occurrence.setResponsibleName(createOccurrenceDto.responsibleName());
-        occurrence.setResolutionDate(createOccurrenceDto.resolutionDate());
-        occurrence.setUserContat(createOccurrenceDto.userContat());
-        occurrence.setOccurrenceFont(createOccurrenceDto.occurrenceFont());
-
-        occurrenceRepository.save(occurrence);
+        occurrenceService.createOccurrence(createOccurrenceDto, token);
 
         return ResponseEntity.ok().build();
     }
 
+    @PutMapping("/occurrences/{id}")
+    public ResponseEntity<Void> updateOccurrence(@PathVariable("id") Long occurrenceId,
+            @RequestBody CreateOccurrenceDto createOccurrenceDto, JwtAuthenticationToken token) throws Exception {
+
+        occurrenceService.updateOccurrence(occurrenceId, createOccurrenceDto, token);
+
+        return ResponseEntity.ok().build();
+
+    }
+
     @DeleteMapping("/occurrences/{id}")
-    public ResponseEntity<Void> deleteOccurrence(@PathVariable("id") Long occurrenceId, JwtAuthenticationToken token) {
-
-        var user = userRepository.findById(UUID.fromString(token.getName()));
-
-        var occurrence = occurrenceRepository.findById(occurrenceId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)); // se nao existir lança excessao
-
-        if (user.get().isAdmin() || occurrence.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
-            occurrenceRepository.deleteById(occurrenceId);
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
+    public ResponseEntity<Void> deleteOccurrence(@PathVariable("id") Long occurrenceId, JwtAuthenticationToken token)
+            throws Exception {
+        occurrenceService.deleteOccurrence(occurrenceId, token);
         return ResponseEntity.ok().build();
     }
 
